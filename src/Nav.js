06f780377a139b7597feddfd96cf87fdb777153a
Shivanshu20472 <shivanshu20472@iiitd.ac.in>
2023-07-15 19:00:46 +0530

    netflix

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg"
+        alt="Netflix Logo"
+      ></img>
+    </div>
+  );
+};
+
+export default Nav;
